d815492a40e026bc4414359bba63ab3520af06dc
z6832732z <ywf281932889@gmail.com>
2025-09-24 12:04:49 +0800

    Update chou.js

--- main/123/chou.js
+++ main/123/chou.js
@@ -1,6 +1,5 @@
 /*
 [rewrite_remote]
-// 拦截博饼接口，模拟对堂奖项
 ^https:\/\/app\.fmcc\.com\.cn\/bass-equity-client-midautumn\/midautumn\/playDice\?math=.* url script-response-body https://raw.githubusercontent.com/ywf281932889-cmd/wan/refs/heads/main/main/123/chou.js
 
 [mitm]
@@ -15,24 +14,14 @@ hostname = app.fmcc.com.cn
     return $done({})
   }
 
-  data = {
-    code: "0000",
-    msg: "成功",
-    data: [
-      {
-        dicePoint: ["3", "3", "3", "3", "5", "6"],
-        dicePointDesc: "对堂",
-        diceAward: 3,
-        recordIs: null,
-        logoUrl: null,
-        prizeName: null,
-        saleName: "朴朴55元券包",
-        squareImageUrl: "https://app.fmcc.com.cn/group2/M00/01/07/CizXIWjD6RmAcx_1AAGF52VmiZs974.png",
-        goodsId: "246447633239368782",
-        relationId: "93"
-      }
-    ],
-    success: true
+  if (data?.code === '0000' && Array.isArray(data.data)) {
+    const item = data.data[0]
+    if (item) {
+      item.dicePoint = ["3", "3", "3", "3", "5", "6"]
+      item.dicePointDesc = "对堂"
+      item.diceAward = 3
+      item.saleName = "朴朴55元券包"
+    }
   }
 
   $done({ body: JSON.stringify(data) })
